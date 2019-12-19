eceba5615b526835f0b78f416090cf5b9c7f7303
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2019-12-19 18:45:46 -0500

    building if statement

--- js/game-logic.js
+++ js/game-logic.js
@@ -1,1 +1,35 @@
-// All code should be written in this file.
+//player one move types
+let playerOneMoveOneType;
+let playerOneMoveTwoType;
+let playerOneMoveThreeType;
+
+//player one value amount
+let playerOneMoveOneValue;
+let playerOneMoveTwoValue;
+let playerOneMoveThreeValue;
+
+//player two move type
+let playerTwoMoveOneType;
+let playerTwoMoveTwoType;
+let playerTwoMoveThreeType;
+
+//player two value
+let playerTwoMoveOneValue;
+let playerTwoMoveTwoValue;
+let playerTwoMoveThreeValue;
+
+const setPlayerMoves = (player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) => {
+     if(!moveOneType || !moveTwoType || !moveThreeType || !moveOneValue || !moveTwoValue || !moveThreeValue) {
+         return;
+     }
+
+     if (!isValidMoveType(moveOneType) || !isValidMoveType(movetwoType) || !isValidMoveType(moveThreeType)) {
+         return;
+     }
+
+
+};
+
+const getRoundWinner = () => {
+
+};
